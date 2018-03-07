fa7410ed030b9541870d972be995035242f6d7f2
Laurynas Montvydas <laurynas.mon@gmail.com>
2018-03-07 18:15:13 +0200

    pabaigta

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const request = require('request');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -8,7 +7,6 @@ const url = 'https://api.bitfinex.com/v1/pubticker/btcusd';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 
 // catch 404 and forward to error handler
@@ -19,7 +17,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
